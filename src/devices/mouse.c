1c34eddeb325b3628865e166fda019dc6ee8c936
Bach Le <bach@bullno1.com>
2025-03-31 05:57:06 +0800

    Add missing mouse device

---
+++ src/devices/mouse.c
@@ -0,0 +1,31 @@
+#include "mouse.h"
+#include "../vm.h"
+
+uint8_t
+buxn_mouse_dei(struct buxn_vm_s* vm, buxn_mouse_t* device, uint8_t address) {
+	switch (address) {
+		case 0x92: return device->x >> 8;
+		case 0x93: return device->x;
+		case 0x94: return device->y >> 8;
+		case 0x95: return device->y;
+		case 0x96: return device->state;
+		case 0x9a: return (uint16_t)device->scroll_x >> 8;
+		case 0x9b: return (uint16_t)device->scroll_x;
+		case 0x9c: return (uint16_t)device->scroll_y >> 8;
+		case 0x9d: return (uint16_t)device->scroll_y;
+		default: return vm->device[address];
+	}
+}
+
+void
+buxn_mouse_deo(struct buxn_vm_s* vm, buxn_mouse_t* device, uint8_t address) {
+	(void)vm;
+	(void)device;
+	(void)address;
+}
+
+void
+buxn_mouse_update(struct buxn_vm_s* vm) {
+	uint16_t vector_addr = buxn_vm_dev_load2(vm, 0x90);
+	if (vector_addr != 0) { buxn_vm_execute(vm, vector_addr); }
+}
---
+++ src/devices/mouse.h
@@ -0,0 +1,36 @@
+#ifndef BUXN_DEVICE_MOUSE_H
+#define BUXN_DEVICE_MOUSE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+struct buxn_vm_s;
+
+typedef struct {
+	uint16_t x;
+	uint16_t y;
+	uint8_t state;
+	int16_t scroll_x;
+	int16_t scroll_y;
+} buxn_mouse_t;
+
+uint8_t
+buxn_mouse_dei(struct buxn_vm_s* vm, buxn_mouse_t* device, uint8_t address);
+
+void
+buxn_mouse_deo(struct buxn_vm_s* vm, buxn_mouse_t* device, uint8_t address);
+
+void
+buxn_mouse_update(struct buxn_vm_s* vm);
+
+static inline void
+buxn_mouse_set_button(buxn_mouse_t* device, uint8_t button, bool down) {
+	uint8_t mask = 1 << button;
+	if (down) {
+		device->state |= mask;
+	} else {
+		device->state ^= mask;
+	}
+}
+
+#endif
