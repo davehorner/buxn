c7d5cbf41a2d89a1314e317e36cd84d7936f8e02
Bach Le <bach@bullno1.com>
2025-04-09 16:24:29 +0800

    Ensure that a non-existent file is only accessed once

--- src/devices/file.c
+++ src/devices/file.c
@@ -2,6 +2,8 @@
 #include "../vm.h"
 #include <string.h>
 
+static char BUXN_FILE_INVALID_HANDLE = 0;
+
 static char*
 buxn_file_read_path(struct buxn_vm_s* vm, uint8_t* mem, char* buf) {
 	uint16_t path_addr = buxn_vm_load2(mem, 0x08, BUXN_DEV_PRIV_ADDR_MASK);
@@ -30,7 +32,9 @@ buxn_file_set_mode(
 	buxn_file_mode_t mode
 ) {
 	if (device->handle != NULL && device->mode != mode) {
-		if (device->stat.type == BUXN_FILE_TYPE_REGULAR) {
+		if (device->handle == &BUXN_FILE_INVALID_HANDLE) {
+			// no op
+		} else if (device->stat.type == BUXN_FILE_TYPE_REGULAR) {
 			buxn_file_fclose(vm, device->handle);
 		} else if (device->stat.type == BUXN_FILE_TYPE_DIRECTORY) {
 			buxn_file_closedir(vm, device->handle);
@@ -48,9 +52,13 @@ buxn_file_set_mode(
 				device->handle = buxn_file_fopen(vm, path, mode);
 			} else if (device->stat.type == BUXN_FILE_TYPE_DIRECTORY) {
 				device->handle = buxn_file_opendir(vm, path);
-			} else if (device ->stat.type == BUXN_FILE_TYPE_INVALID && mode != BUXN_FILE_MODE_READ) {
-				device->handle = buxn_file_fopen(vm, path, mode);
-				device->stat.type = BUXN_FILE_TYPE_REGULAR;
+			} else if (device ->stat.type == BUXN_FILE_TYPE_INVALID) {
+				if (mode != BUXN_FILE_MODE_READ) {
+					device->handle = buxn_file_fopen(vm, path, mode);
+					device->stat.type = BUXN_FILE_TYPE_REGULAR;
+				} else {
+					device->handle = &BUXN_FILE_INVALID_HANDLE;
+				}
 			}
 			device->mode = mode;
 		}
@@ -145,73 +153,69 @@ buxn_file_deo(struct buxn_vm_s* vm, buxn_file_t* device, uint8_t* mem, uint8_t p
 		case 0x0d: {
 			// read
 			buxn_file_handle_t* file = buxn_file_set_mode(vm, device, mem, BUXN_FILE_MODE_READ);
-			if (file != NULL) {
-				uint16_t read_addr = buxn_vm_load2(mem, port - 1, BUXN_DEV_PRIV_ADDR_MASK);
-				uint16_t length = buxn_file_clamp_length(
-					buxn_vm_load2(mem, 0xa, BUXN_DEV_PRIV_ADDR_MASK),
-					read_addr
-				);
-				uint16_t total_bytes_read = 0;
+			uint16_t read_addr = buxn_vm_load2(mem, port - 1, BUXN_DEV_PRIV_ADDR_MASK);
+			uint16_t length = buxn_file_clamp_length(
+				buxn_vm_load2(mem, 0xa, BUXN_DEV_PRIV_ADDR_MASK),
+				read_addr
+			);
+			uint16_t total_bytes_read = 0;
 
-				if (device->stat.type == BUXN_FILE_TYPE_DIRECTORY) {
-					// Read dir
-					uint16_t read_dir_pos = device->read_dir_pos;
-					uint16_t read_dir_len = device->read_dir_len;
-					char* read_dir_buf = device->read_dir_buf;
-					while (length > 0) {
-						if (read_dir_pos >= read_dir_len) {
-							// Refill read dir buffer
-							read_dir_pos = 0;
-							read_dir_len = 0;
-							buxn_file_stat_t stat;
-							const char* filename;
-							if ((filename = buxn_file_readdir(vm, file, &stat)) == NULL) {
-								break;
-							}
-
-							buxn_file_format_stat(read_dir_buf, 4, &stat);
-							read_dir_len += 4;
-
-							size_t name_len = strlen(filename);
-							if (read_dir_len + name_len + 2 > sizeof(device->read_dir_buf)) {
-								read_dir_len = 0;
-								break;
-							} else {
-								read_dir_buf[read_dir_len++] = ' ';
-								memcpy(&read_dir_buf[read_dir_len], filename, name_len);
-								read_dir_len += (uint16_t)name_len;
-								read_dir_buf[read_dir_len++] = '\n';
-							}
+			if (device->stat.type == BUXN_FILE_TYPE_DIRECTORY) {
+				// Read dir
+				uint16_t read_dir_pos = device->read_dir_pos;
+				uint16_t read_dir_len = device->read_dir_len;
+				char* read_dir_buf = device->read_dir_buf;
+				while (length > 0) {
+					if (read_dir_pos >= read_dir_len) {
+						// Refill read dir buffer
+						read_dir_pos = 0;
+						read_dir_len = 0;
+						buxn_file_stat_t stat;
+						const char* filename;
+						if ((filename = buxn_file_readdir(vm, file, &stat)) == NULL) {
+							break;
 						}
 
-						uint16_t bytes_available = read_dir_len - read_dir_pos;
-						uint16_t bytes_read = length < bytes_available ? length : bytes_available;
-						memcpy(&vm->memory[read_addr], &read_dir_buf[read_dir_pos], bytes_read);
-						read_addr += bytes_read;
-						total_bytes_read += bytes_read;
-						read_dir_pos += bytes_read;
-						length -= bytes_read;
+						buxn_file_format_stat(read_dir_buf, 4, &stat);
+						read_dir_len += 4;
+
+						size_t name_len = strlen(filename);
+						if (read_dir_len + name_len + 2 > sizeof(device->read_dir_buf)) {
+							read_dir_len = 0;
+							break;
+						} else {
+							read_dir_buf[read_dir_len++] = ' ';
+							memcpy(&read_dir_buf[read_dir_len], filename, name_len);
+							read_dir_len += (uint16_t)name_len;
+							read_dir_buf[read_dir_len++] = '\n';
+						}
 					}
 
-					device->read_dir_pos = read_dir_pos;
-					device->read_dir_len = read_dir_len;
-				} else {
-					// Read file
-					uint16_t bytes_read;
-					while (
-						length > 0
-						&& (bytes_read = buxn_file_fread(vm, file, &vm->memory[read_addr], length)) != 0
-					) {
-						total_bytes_read += bytes_read;
-						read_addr += bytes_read;
-						length -= bytes_read;
-					}
+					uint16_t bytes_available = read_dir_len - read_dir_pos;
+					uint16_t bytes_read = length < bytes_available ? length : bytes_available;
+					memcpy(&vm->memory[read_addr], &read_dir_buf[read_dir_pos], bytes_read);
+					read_addr += bytes_read;
+					total_bytes_read += bytes_read;
+					read_dir_pos += bytes_read;
+					length -= bytes_read;
 				}
 
-				device->success = total_bytes_read;
-			} else {
-				device->success = 0;
+				device->read_dir_pos = read_dir_pos;
+				device->read_dir_len = read_dir_len;
+			} else if (device->stat.type == BUXN_FILE_TYPE_REGULAR) {
+				// Read file
+				uint16_t bytes_read;
+				while (
+					length > 0
+					&& (bytes_read = buxn_file_fread(vm, file, &vm->memory[read_addr], length)) != 0
+				) {
+					total_bytes_read += bytes_read;
+					read_addr += bytes_read;
+					length -= bytes_read;
+				}
 			}
+
+			device->success = total_bytes_read;
 		} break;
 		case 0x0f: {
 			// write
